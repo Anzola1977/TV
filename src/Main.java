3f80e2c984e159eeef16c1ed399086b56aa02e75
Andrew Zola <and.zola.77@gmail.com>
2022-10-23 20:15:44 +0200

    First commit with inner class

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         TVset tv = new TVset();
         TVset.Remote remote = tv.new Remote();
+        //TVset.Remote remote2 = new TVset().new Remote();
         remote.setTVset(tv);
         System.out.println(tv);
         remote.turnOn();
+        System.out.println(tv);
+        remote.setChannel(4);
         System.out.println(tv);
         remote.setChannel(2);
         System.out.println(tv);
